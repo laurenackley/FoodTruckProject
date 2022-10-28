7e0ff95b0fea61131c5e93c8b56b9a47403f7143
laurenackley <lackley127@gmail.com>
2022-10-28 17:25:08 -0500

    start project, confirm working

---
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.foodtruck.app;
+
+public class FoodTruckApp {
+	public static void main(String[] args) {
+	// that is the only static allowed in the project. 
+}
+	
+	//menu
+	//scanner
+	//what they want to do - part of menu 
+}
---
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.foodtruck.entities;
+
+public class FoodTruck {
+	private static int nextTruckId; // don't pass as a ID field ONLY other static 
+	private String name; 
+	private int id; 
+	
+	
+}
