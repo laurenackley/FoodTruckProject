4af430c7a54d4ffe4d293d7fb928471dad6f8faa
laurenackley <lackley127@gmail.com>
2022-10-29 17:53:15 -0500

    Not completed, saving before testing new

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,11 +1,120 @@
 package com.skilldistillery.foodtruck.app;
 
+import com.skilldistillery.foodtruck.entities.FoodTruck;
+
 public class FoodTruckApp {
+	java.util.Scanner sc = new java.util.Scanner(System.in);
+
+	private FoodTruck[] FleetTrucks = new FoodTruck[5];
+	private String name;
+
 	public static void main(String[] args) {
-	// that is the only static allowed in the project. 
-}
-	
-	//menu
-	//scanner
-	//what they want to do - part of menu 
+		// that is the only static allowed in the project.
+		System.out.println("Introduction .... "); /*Fix later*/
+		FoodTruckApp fTA = new FoodTruckApp();
+		fTA.TruckData();
+		fTA.MenuChoices();
+	}
+
+	public  void TruckData() {
+		// create menu and store the information as an object and add to an array
+		// needs to have an ID assigned to it but not by the user but by food truck
+		// constructor
+		// from the static field and incremented as each truck is created
+
+		for (int i  = 0; i < 5; i++) {
+
+			if (i == 0) {
+				System.out.println("Please enter the name of a food truck.");
+				name = sc.nextLine();
+			} else {
+				System.out.println("Please enter the name of another food truck or enter quit if you'd like to stop.");
+				name = sc.nextLine();
+				if (name.equalsIgnoreCase("quit")) {
+					break;
+				}
+			}
+			System.out.println("What would you rate them out of 5? You can use decimals if you would like.");
+			int ratingNum = sc.nextInt();
+			sc.nextLine();
+
+			System.out.println("What type of food does " + name + " serve?");
+			String foodType = sc.nextLine();
+			// menu
+			// scanner
+			// what they want to do - part of menu
+
+//If user enters quit then it needs to end immediately but program continues
+			FoodTruck truck = new FoodTruck(name, ratingNum, foodType);
+			FleetTrucks[i] = truck;
+		}
+	}
+
+	public void MenuChoices() {
+		int choice = 0;
+		while (choice != 4) {
+			System.out.println("What would you like to do?");
+			System.out.println("1. List all existing food trucks");
+			System.out.println("2. View the average rating of food trucks.");
+			System.out.println("3. Display the highest-rated food truck.");
+			System.out.println("4. Quit");
+			choice = sc.nextInt();
+			sc.nextLine();
+			switch (choice) {
+			case 1:
+				for(int i = 0; i < FleetTrucks.length; i++) {
+					if(FleetTrucks[i] != null) {
+						System.out.println(FleetTrucks[i].toString());	
+				System.out.println("---------------------------------------------------------");
+					}
+				}
+				break;
+			case 2:
+				double number = 0;
+				double numOfTrucks = 0;
+				double average = 0;
+				for(int i =0; i < FleetTrucks.length; i++) {
+					if(FleetTrucks[i] != null) {
+						number += (FleetTrucks[i].getRatingNum());
+						numOfTrucks++;
+					}
+					else {
+						number += (FleetTrucks[i].getRatingNum());
+						numOfTrucks++;
+						
+					}
+				}
+				average = number/numOfTrucks;
+				System.out.println("The average rating of the food trucks are " + average );
+				break;
+
+			case 3:
+				int ratings = 0;
+				int numTrucks = 0;
+				int highest = 0;
+				for (int i = 0; i < FleetTrucks.length; i++) {
+					if(FleetTrucks[i] != null) {
+						ratings = FleetTrucks[i].getRatingNum();
+						numTrucks++;
+				}
+				}
+					highest = ratings;
+				System.out.println("The highest rated food truck is " + highest);
+				
+				break;
+
+			case 4:
+				System.out.println("Thanks for playing, have a great day. Program ending...");
+				break; 
+				
+			default:
+				System.out.println("You have selected an invalid option. Please try again.");
+				break;
+			}
+			if (choice == 4) {
+				break;
+			}
+		}
+
+	}
 }
--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -1,9 +1,68 @@
 package com.skilldistillery.foodtruck.entities;
 
 public class FoodTruck {
-	private static int nextTruckId; // don't pass as a ID field ONLY other static 
+	private static int numberOfTrucks = 0; // don't pass as a ID field ONLY other static 
 	private String name; 
-	private int id; 
+	private int ratingNum;
+	private String foodType;
+	private int truckID;
 	
+	public FoodTruck() {
+	}
 	
+	public FoodTruck (String name, int ratingNum, String foodType) {
+		this.name = name;
+		this.ratingNum = ratingNum;
+		this.foodType = foodType;
+		numberOfTrucks++;
+		truckID = numberOfTrucks;
+	}
+	
+	public static int getNumberOfTrucks() {
+		return numberOfTrucks;
+	}
+
+	public static void setNumberOfTrucks(int numberOfTrucks) {
+		FoodTruck.numberOfTrucks = numberOfTrucks;
+	}
+
+	public int getTruckID() {
+		return truckID;
+	}
+
+	public void setTruckID(int truckID) {
+		this.truckID = truckID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getRatingNum() {
+		return ratingNum;
+	}
+
+	public void setRatingNum(int ratingNum) {
+		this.ratingNum = ratingNum;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	@Override
+	public String toString() {
+		return "FoodTruck: " + name + "\nRating: " + ratingNum + "\nFoodType:" + foodType + "\ntruckID:"
+				+ truckID;
+	}
+
+
 }
